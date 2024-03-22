341b80e2b9d8dac5b89e29ab4c9c808a55f479ea
Justin Onyango <ojustin446@gmail.com>
2024-03-22 14:05:16 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution here!
+// Write your solutions here
+const cats=["Milo","Otis", "Garfield"];
+
+console.log(cats)
+
+const destructivelyAppendCat=(name)=> cats.push("Ralph");
+
+const destructivelyPrependCat=(name)=>cats.unshift("Bob");
+
+const destructivelyRemoveLastCat=(name)=>cats.pop();
+
+const destructivelyRemoveFirstCat=(name)=>cats.shift();
+
+const appendCat=(name)=>[...cats,"Broom"];
+
+const prependCat=(name)=>["Arnold",...cats];
+
+const removeLastCat=(name)=>cats.slice(0,2);
+
+const removeFirstCat=(name)=>cats.slice(1)
